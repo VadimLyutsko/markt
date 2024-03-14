20c8ef2de93cd13fdb7b94c6da3e96297df9c2de
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2024-03-14 12:53:37 +0300

    test

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  output: 'export', // Add basePath
+  basePath: '/github-pages',
+};
+
+export default nextConfig;
